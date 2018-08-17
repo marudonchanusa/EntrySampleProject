1bfff480ba25cde514db01e20b151413db972982
yoshida <yoshida@NPC-075SFCLTP.e-cometrue.net>
2018-08-17 15:24:16 +0900

    v-layout試し

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -1,49 +1,20 @@
 <template>
   <v-container fluid>
     <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-toolbar color="indigo" dark>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-
-            <v-toolbar-title>Inbox</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-            <v-btn icon>
-              <v-icon>search</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-              <v-icon>more_vert</v-icon>
-            </v-btn>
-          </v-toolbar>
-          <v-list>
-            <v-list-tile
-              v-for="item in items"
-              :key="item.title"
-              avatar
-              @click=""
-            >
-              <v-list-tile-action>
-                <v-icon v-if="item.icon" color="pink">star</v-icon>
-              </v-list-tile-action>
-
-              <v-list-tile-content>
-                <v-list-tile-title v-text="item.title"></v-list-tile-title>
-              </v-list-tile-content>
-
-              <v-list-tile-avatar>
-                <img :src="item.avatar">
-              </v-list-tile-avatar>
-            </v-list-tile>
-          </v-list>
-        </v-card>
+      <v-flex md12>
+        <span>this is dash board</span>
+        <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
       </v-flex>
     </v-layout>
-
-    <span>this is dash board</span>
-    <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
+    <v-layout row>
+      <v-flex md12>
+        <span>this is dash board</span>
+        <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <span>Hello world</span>
+    </v-layout>
   </v-container>
 </template>
 
@@ -52,16 +23,4 @@
 </style>
 
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          { icon: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
-          { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-          { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-          { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' }
-        ]
-      }
-    }
-  }
 </script>
